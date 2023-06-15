820284d3ccd82ef8034b54729b5f51d44fee644a
sasaki-ryunosuke <tigadainagaia@icloud.com>
2023-06-15 12:29:04 +0900

    Initial Commit

---
+++ Realm_test/Realm_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Realm_testApp.swift
+//  Realm_test
+//
+//  Created by x19048xx on 2023/06/15.
+//
+
+import SwiftUI
+
+@main
+struct Realm_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Realm_testUITests/Realm_testUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Realm_testUITestsLaunchTests.swift
+//  Realm_testUITests
+//
+//  Created by x19048xx on 2023/06/15.
+//
+
+import XCTest
+
+final class Realm_testUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
