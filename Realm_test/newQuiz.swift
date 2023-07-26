a71170747b2f89bbc79cdcb48bbc5f007d0eff88
sasaki-ryunosuke <tigadainagaia@icloud.com>
2023-07-26 17:47:58 +0900

    newquizの実装途中

--- Realm_test/newQuiz.swift
+++ Realm_test/newQuiz.swift
@@ -58,14 +58,41 @@ import SwiftUI
 //    }
 //}
 
+//データベースのデータはuuid、word,noteDescription,tagの４要素からなる
+//「今まで記録したことを思い出してみましょう！」、「クイズを始める」ボタンの2つを配置した
+//クイズを始めるボタンを押すと以下の処理が始まり、ビュー２へ
+//ボタンを押したときに、データベースからデータを取得し、配列にデータを格納する処理を行う。この配列はクイズの問題を表示するために使用する
+//「この説明が示す、あなたがメモした言葉は？」「notedescription」「入力フォーム」「解答ボタン」の４つを表示するビュー２
+//解答ボタンを押すと、入力された言葉とwordが一致した場合は正解、不一致の場合は不正解と表示し、「次の問題へ」というボタンも配置されたビュー3がポップアップする
+//次の問題へボタンを押すと、問題を表示する配列の番号を指すインデックスが加算され、ビュー３が閉じて、ビュー２には次の問題が表示される。
+//インデックスが最後に到達する(クイズが全て終了する)と、「クイズを終了します。」と表示され、ビュー１へ戻る
 
-//クイズのタイトル、「今まで記録したことを思い出してみましょう！」、「クイズを始める」ボタンを配置したビュー
-//ボタンを押したときに、データベースからデータを取得し、クイズ出題のための配列にデータを格納する処理を行う
-//「この説明が示す、あなたがメモした言葉は？」「説明」「入力フォーム」を表示する
-//ブランチ分岐
+
+struct protQuizStartView: View {
+    var body: some View{
+        VStack{
+            Text("今までメモしたことを思い出してみましょう")
+            Button("クイズを始める", action: getquiz)
+        }
+    }
+    func getquiz() {
+        @State private var notes: [Note] = {
+            do {
+                let realm = try Realm()
+                let results = realm.objects(Note.self)
+                let shuffledResults = Array(results).shuffled()
+                let selectedNotes = shuffledResults.prefix(5)
+                return Array(selectedNotes)
+            } catch {
+                print("Failed to access Realm: \(error)")
+                return []
+            }
+        }()
+    }
+}
+
 
 struct protQuizView: View {
-    //クイズ内容の取得処理
     @State private var notes: [Note] = {
         do {
             let realm = try Realm()
@@ -78,46 +105,107 @@ struct protQuizView: View {
             return []
         }
     }()
-    @State private var selectedNote: Note?
-    @State private var showingDetail = false
-
-    //配列の内容表示
+    
+    @State private var currentNoteIndex = 0
+    @State private var answer = ""
+    @State private var showAnswer = false
+    @State private var isCorrect = false
+    
     var body: some View {
-        List {
-            ForEach(notes, id: \.id) { note in
-                Text(note.word)
-                    .onTapGesture {
-                        self.selectedNote = note
-                        self.showingDetail = true
+        if currentNoteIndex < notes.count {
+            VStack {
+                Text("この説明が示す、あなたがメモした言葉は？")
+                Text(notes[currentNoteIndex].noteDescription)
+                TextField("あなたの回答", text: $answer)
+                Button("解答", action: checkAnswer)
+                
+                if showAnswer {
+                    if isCorrect {
+                        Text("正解！")
+                    } else {
+                        Text("不正解。")
                     }
+                    
+                    Button("次の問題へ", action: nextQuestion)
+                }
             }
+        } else {
+            Text("クイズを終了します。")
+            Button("再開", action: restartQuiz)
         }
-        .sheet(isPresented: $showingDetail) {
-            if let note = self.selectedNote {
-                NoteDetailView(note: note, isPresented: self.$showingDetail)
-            }
-        }
+    }
+    
+    func checkAnswer() {
+        showAnswer = true
+        isCorrect = (answer == notes[currentNoteIndex].word)
+    }
+    
+    func nextQuestion() {
+        answer = ""
+        showAnswer = false
+        currentNoteIndex += 1
+    }
+    
+    func restartQuiz() {
+        currentNoteIndex = 0
     }
 }
 
-//詳細表示
-struct NoteDetailView: View {
-    var note: Note
-    @Binding var isPresented: Bool  // 表示状態を制御する変数
 
-    var body: some View {
-        VStack(alignment: .leading) {
-            Text(note.word).font(.title)
-            Text(note.noteDescription).font(.body)
-            Text(note.tag).font(.body)
-            
-            // 閉じるボタン
-            Button(action: {
-                self.isPresented = false
-            }) {
-                Text("閉じる")
-            }
-        }
-        .padding()
-    }
-}
+//struct protQuizView: View {
+//    //クイズ内容の取得処理
+//    @State private var notes: [Note] = {
+//        do {
+//            let realm = try Realm()
+//            let results = realm.objects(Note.self)
+//            let shuffledResults = Array(results).shuffled()
+//            let selectedNotes = shuffledResults.prefix(5)
+//            return Array(selectedNotes)
+//        } catch {
+//            print("Failed to access Realm: \(error)")
+//            return []
+//        }
+//    }()
+//    @State private var selectedNote: Note?
+//    @State private var showingDetail = false
+//
+//    //配列の内容表示
+//    var body: some View {
+//        List {
+//            ForEach(notes, id: \.id) { note in
+//                Text(note.word)
+//                    .onTapGesture {
+//                        self.selectedNote = note
+//                        self.showingDetail = true
+//                    }
+//            }
+//        }
+//        .sheet(isPresented: $showingDetail) {
+//            if let note = self.selectedNote {
+//                NoteDetailView(note: note, isPresented: self.$showingDetail)
+//            }
+//        }
+//    }
+//}
+//
+////詳細表示
+//struct NoteDetailView: View {
+//    var note: Note
+//    @Binding var isPresented: Bool  // 表示状態を制御する変数
+//
+//    var body: some View {
+//        VStack(alignment: .leading) {
+//            Text(note.word).font(.title)
+//            Text(note.noteDescription).font(.body)
+//            Text(note.tag).font(.body)
+//
+//            // 閉じるボタン
+//            Button(action: {
+//                self.isPresented = false
+//            }) {
+//                Text("閉じる")
+//            }
+//        }
+//        .padding()
+//    }
+//}
